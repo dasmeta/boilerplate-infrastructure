8389b99ffa94e8fd379418fcd60ce1b1316e251d
Karapetyan, Aram <aram@dasmeta.com>
2023-09-05 15:00:35 +0400

    Copy from test infra to release

--- _metacloud.tf
+++ _metacloud.tf
@@ -1,20 +1,40 @@
 terraform {
   cloud {
-    organization = "boilerplate-organisation"
-    workspaces {
-      name = "boilerplate-infrastructure"
-    }
+    organization = "boilerplate-testing"
+    workspaces { name = "boilerplate-infrastructure" }
   }
 }
 
+variable "tfc_org" {}
+variable "tfc_workspace" {}
+variable "tfc_token" {}
+variable "git_provider" {}
+variable "git_org" {}
+variable "git_repo" {}
+variable "git_token" {}
+variable "default_region" {}
+variable "access_key_id" {}
+variable "secret_access_key" {}
+
 module "metacloud" {
-  source = "github.com/dasmeta/terraform-tfe-cloud.git?ref=DMVP-2598-simplify-tfe-use"
+  source  = "dasmeta/cloud/tfe"
+  version = "v2.0.2"
 
-  org   = "boilerplate-organisation"
-  token = "..." # terraform cloud token
+  org   = var.tfc_org
+  token = var.tfc_token
 
-  git_provider = "github"
-  git_org      = "boilerplate-organisation"
-  git_repo     = "boilerplate-infrastructure"
-  git_token    = "..." # github oauth token
+  rootdir   = "${path.module}/_terraform/"
+  targetdir = "${path.module}/_terraform"
+  yamldir   = "${path.module}/."
+
+  git_provider = var.git_provider
+  git_org      = var.git_org
+  git_repo     = var.git_repo
+  git_token    = var.git_token
+
+  aws = {
+    access_key_id     = var.access_key_id
+    secret_access_key = var.secret_access_key
+    default_region    = var.default_region
+  }
 }
