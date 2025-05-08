401b409b8c1c299dc12b5f486eb13be36357f5c6
Vazgen Sargsyan <vazgen@dasmeta.com>
2025-05-08 14:36:57 +0400

    fix(demo) Grafana

--- _terraform/2-products/prod/grafana/main.tf
+++ _terraform/2-products/prod/grafana/main.tf
@@ -8,7 +8,7 @@ module "this" {
   aws_region = "eu-central-1"
   cluster_name = "eks-prod"
   grafana_admin_password = "password"
-  grafana_configs = {"datasources":[{"name":"cloudwatch","type":"cloudwatch"}],"ingress":{"annotations":{"alb.ingress.kubernetes.io/group.name":"prod-ingress"},"hosts":["grafana.demo.dasmeta.com"]},"persistence":{"size":"10Gi","storage_class":"gp2"},"redundency":{"enabled":true},"resources":{"limits":{"cpu":"1","mem":"2Gi"},"request":{"cpu":"1","mem":"2Gi"}}}
+  grafana_configs = {"datasources":[{"name":"cloudwatch","type":"cloudwatch"}],"ingress":{"annotations":{"alb.ingress.kubernetes.io/group.name":"prod-ingress"},"hosts":["grafana.demo.dasmeta.com"]},"persistence":{"size":"10Gi","storage_class":"gp2"},"redundency":{"enabled":false},"resources":{"limits":{"cpu":"1","mem":"2Gi"},"request":{"cpu":"1","mem":"2Gi"}}}
   loki_configs = {"enabled":true}
   name = "demo-prod-grafana"
   prometheus_configs = {"enabled":true,"replicas":1,"resources":{"limit":{"cpu":"1500m","mem":"1.5Gi"},"request":{"cpu":"1","mem":"1Gi"}},"storage_class":"gp2","storage_size":"10Gi"}
