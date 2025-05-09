e37e3e2b7ee9e84f1d56305810b13592dbc52c18
Vazgen Sargsyan <vazgen@dasmeta.com>
2025-05-09 12:43:36 +0400

    Fix(demo) ingress secure

--- _terraform/1-environments/prod/ingress/main.tf
+++ _terraform/1-environments/prod/ingress/main.tf
@@ -6,7 +6,7 @@ module "this" {
 
   additional_hostnames = ["*.demo.dasmeta.com"]
   alarms = {"enabled":false,"sns_topic":"Default"}
-  certificate_arn = "arn:aws:acm:eu-central-1:817342036967:certificate/dc6e23c5-a0a4-45d2-ac39-81ee643619fe"
+  certificate_arn = "${data.tfe_outputs.this["1-environments/prod/certificate"].values.results.arn}"
   healthcheck_path = "/"
   healthcheck_success_codes = "200-399"
   hostname = "demo.dasmeta.com"
@@ -18,7 +18,7 @@ module "this" {
 
 
 data "tfe_outputs" "this" {
-  for_each = { for workspace in ["1-environments/prod/eks"] : workspace => workspace }
+  for_each = { for workspace in ["1-environments/prod/eks","1-environments/prod/certificate"] : workspace => workspace }
 
   organization = "Demo-Dasmeta"
   workspace    = replace(each.value, "/[^a-zA-Z0-9_-]+/", "_")
